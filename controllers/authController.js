55218be4802591327b87d0fd83506d00db99625e
Manish Sahu <sahum6703@gmail.com>
2023-10-26 14:35:56 +0530

    final code
    ecommerce mern stack app

---
+++ controllers/authController.js
@@ -0,0 +1,249 @@
+import userModel from "../models/userModel.js";
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
+import JWT from "jsonwebtoken";
+import orderModel from "../models/orderModel.js";
+
+export const registerController = async (req, res) => {
+    try {
+        const {name, email, password, phone, address, answer} = req.body;
+        if(!name){
+            return res.send({message: 'Name is required'});
+        }
+        if(!email){
+            return res.send({message: 'Email is required'});
+        }
+        if(!password){
+            return res.send({message: 'Password is required'});
+        }
+        if(!phone ){
+            return res.send({message: 'Phone is required'});
+        }
+        if(!address){
+            return res.send({message: 'Address is required'});
+        }
+        if(!answer){
+            return res.send({message: 'Answer is required'});
+        }
+
+        // check if user exists
+        const existingUser = await userModel.findOne({email});
+
+        if(existingUser){
+            return res.status(200).send({
+                success: false,
+                message: 'Already registered please login',
+            })
+        }
+
+        // register user
+        const hashedPassword = await hashPassword(password)
+
+        // save
+        const user = await new userModel({name,email,phone,address, password:hashedPassword, answer }).save()
+
+        res.status(201).send({
+            success:true,
+            message:"User registered successfully",
+            user,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Error in Registration',
+            error
+        })
+    }
+};
+
+// POST LOGIN
+export const loginController = async(req,res) => {
+    try {
+        const {email, password} = req.body;
+        if(!email  || !password){
+            return res.status(404).send({
+                success:false,
+                message:"Invalid email or password"
+            });
+        }
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.status(404).send({
+                success:false,
+                message:"Email is not registered"
+            });
+        }
+        const match = await comparePassword(password, user.password);
+        if(!match){
+            return res.status(404).send({
+                success:false,
+                message:"Invalid password"
+            });
+        }
+        // creating token
+        const token = await JWT.sign({_id:user._id}, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
+        res.status(200).send({
+            success:true,
+            message:"Login successful",
+            user:{
+                _id: user._id,
+                name: user.name,
+                email : user.email,
+                phone: user.phone,
+                address: user.address,
+                role: user.role
+            },
+            token,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message:"Error in login",
+            error
+        })
+    }
+};
+
+// forgot password
+export const forgotPasswordController = async (req, res) => {
+    try {
+      const { email, answer, newPassword } = req.body;
+      if (!email) {
+        res.status(400).send({ message: "Email is required" });
+      }
+      if (!answer) {
+        res.status(400).send({ message: "Answer is required" });
+      }
+      if (!newPassword) {
+        res.status(400).send({ message: "New Password is required" });
+      }
+      //check
+      const user = await userModel.findOne({ email, answer });
+      //validation
+      if (!user) {
+        return res.status(404).send({
+          success: false,
+          message: "Wrong Email Or Answer",
+        });
+      }
+      const hashed = await hashPassword(newPassword);
+      await userModel.findByIdAndUpdate(user._id, { password: hashed });
+      res.status(200).send({
+        success: true,
+        message: "Password Reset Successfully",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        success: false,
+        message: "Something went wrong",
+        error,
+      });
+    }
+};
+
+export const testController = async(req,res) => {
+    try {
+        res.send("Protected Routes");
+    } catch (error) {
+        console.log(error);
+        res.send({ error });
+    }
+};
+
+//update profile
+export const updateProfileController = async (req, res) => {
+    try {
+      const { name, email, password, address, phone } = req.body;
+      const user = await userModel.findById(req.user._id);
+      //password
+      if (password && password.length < 3) {
+        return res.json({ error: "Password is required and 6 character long" });
+      }
+      const hashedPassword = password ? await hashPassword(password) : undefined;
+      const updatedUser = await userModel.findByIdAndUpdate(
+        req.user._id,
+        {
+          name: name || user.name,
+          password: hashedPassword || user.password,
+          phone: phone || user.phone,
+          address: address || user.address,
+        },
+        { new: true }
+      );
+      res.status(200).send({
+        success: true,
+        message: "Profile Updated SUccessfully",
+        updatedUser,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(400).send({
+        success: false,
+        message: "Error While Update profile",
+        error,
+      });
+    }
+};
+
+
+//orders
+export const getOrdersController = async (req, res) => {
+  try {
+    const orders = await orderModel
+      .find({ buyer: req.user._id })
+      .populate("products", "-photo")
+      .populate("buyer", "name");
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error While Getting Orders",
+      error,
+    });
+  }
+};
+
+//orders
+export const getAllOrdersController = async (req, res) => {
+  try {
+    const orders = await orderModel
+      .find({})
+      .populate("products", "-photo")
+      .populate("buyer", "name")
+      .sort({ createdAt: "-1" });
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error While Getting Orders",
+      error,
+    });
+  }
+};
+
+//order status
+export const orderStatusController = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+    const orders = await orderModel.findByIdAndUpdate(
+      orderId,
+      { status },
+      { new: true }
+    );
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error While Updating Order",
+      error,
+    });
+  }
+};
---
+++ routes/productRoutes.js
@@ -0,0 +1,68 @@
+import express from "express";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import { 
+    createProductController, 
+    getProductController, 
+    getSingleProductController, 
+    productPhotoController, 
+    deleteProductController,
+    productCategoryController, 
+    productFiltersController,
+    updateProductController,
+    productCountController,
+    productListController,
+    searchProductController, 
+    relatedProductController,
+    brainTreeTokenController,
+    brainTreePaymentController} 
+    from "../controllers/productController.js";
+import formidable from "express-formidable";
+
+const router = express.Router();
+
+router.post('/create-product', requireSignIn, isAdmin, formidable(), createProductController);
+
+// get products
+router.get('/get-product', getProductController);
+
+// single product
+router.get('/get-product/:slug', getSingleProductController);
+
+// get photo
+router.get('/product-photo/:pid', productPhotoController);
+
+// delete product
+router.delete('/delete-product/:pid', deleteProductController);
+
+// update product
+router.put('/update-product/:pid', requireSignIn, isAdmin, formidable(), updateProductController);
+
+// category wise product
+router.get("/product-category/:slug", productCategoryController);
+
+// filter product
+router.post('/product-filters', productFiltersController);
+
+// prod count
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
+
+// search prod
+router.get("/search/:keyword", searchProductController);
+
+//similar product
+router.get("/related-product/:pid/:cid", relatedProductController);
+
+//category wise product
+router.get("/product-category/:slug", productCategoryController);
+
+// payments routes
+// token
+router.get('/braintree/token',  brainTreeTokenController);
+
+//payments
+router.post("/braintree/payment", requireSignIn, brainTreePaymentController);
+
+export default router;
