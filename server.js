5bd5bb16d28cee6c263609f760da886ba23c35b3
Manish Sahu <sahum6703@gmail.com>
2023-10-26 14:38:25 +0530

    initial commit

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoutes from './routes/authRoute.js'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors';
+import path from 'path';
 // configure env
 dotenv.config();
 
@@ -20,7 +21,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-// app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 // routes
 app.use('/api/v1/auth', authRoutes);
@@ -28,13 +29,13 @@ app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes);
 
 // rest api
-// app.use('*', function(req, res){
-//     res.sendFile(path.join(__dirname, './client/build/index.html')); 
-// });
-
-app.get('/', (req, res) => {
-    res.send("<h1>Welcome to ecommerce app"); 
+app.use('*', function(req, res){
+    res.sendFile(path.join(__dirname, './client/build/index.html')); 
 });
+
+// app.get('/', (req, res) => {
+//     res.send("<h1>Welcome to ecommerce app"); 
+// });
 
 // PORT
 const PORT = process.env.PORT || 8080;
